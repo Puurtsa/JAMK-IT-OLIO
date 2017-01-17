23c3375ba79831a5b06a43c744f2313d2394519f
K9091 <K9091@student.jamk.fi>
2017-01-17 17:11:12 +0200

    1 2 ja 3

---
+++ BlackJackV0/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Tehtava1()
+        {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
+            Console.WriteLine("Anna numero: 1, 2 tai 3");
+            string retval = Console.ReadLine();
+            int luku = int.Parse(retval);
+            if (luku == 1)
+            {
+                retval = "yksi";
+            }
+            else if (luku == 2)
+            {
+                retval = "kaksi";
+            }
+            else if (luku == 3)
+            {
+                retval = "kolme";
+            }
+            else
+            {
+                retval = "joku muu luku";
+            }
+            //tulostus käyttäjälle
+            Console.WriteLine(retval);
+        }
+    }
+}
